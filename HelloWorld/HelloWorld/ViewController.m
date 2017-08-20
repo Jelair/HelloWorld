5ce42464815b7ce878ea608c07ddede2605f3ee1
NowOrNever <nowornever@NowOrNeverdeMac.local>
2017-08-20 12:25:42 +0800

    first version

---
+++ HelloWorld/HelloWorld/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HelloWorld
+//
+//  Created by NowOrNever on 20/08/2017.
+//  Copyright © 2017 NowOrNever. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
